fffc56907eb63cddc417f9d44c80cd1d2115147d
philipplibowicz <philipplibowicz@gmail.com>
2025-05-07 15:08:44 +0200

    M006,M007 done

--- M006_Lab/Program.cs
+++ M006_Lab/Program.cs
@@ -2,7 +2,72 @@
 {
     public class Fahrzeug
     {
+        // Eigenschaften
+        public string Name { get; set; }
+        public int MaxGeschwindigkeit { get; set; }
+        public int AktGeschwindigkeit { get; set; }
+        public double Preis {  get; set; }
+        public bool MotorLaeuft {  get; set; }
 
+        // Konstruktor
+        public Fahrzeug(string name, int maxG, double preis)
+        {
+            this.Name = name;
+            this.MaxGeschwindigkeit = maxG;
+            this.Preis = preis;
+            this.AktGeschwindigkeit = 0;
+            this.MotorLaeuft = false;
+        }
+
+        // Info
+        public string Info()
+        {
+            if (this.MotorLaeuft)
+                return $"{this.Name} kostet {this.Preis}€ und fährt momentan mit {this.AktGeschwindigkeit} von maximal {this.MaxGeschwindigkeit}";
+            else 
+                return $"{this.Name} kostet {this.Preis}€und könnte maximal {this.MaxGeschwindigkeit}km/h fahren.";
+        }
+
+        // StarteMotor
+        public void StarteMotor()
+        {
+            if(this.MotorLaeuft)
+                Console.WriteLine($"Der Motor von {this.Name} läuft bereits");
+            else
+            {
+                this.MotorLaeuft = true;
+                Console.WriteLine($"Der Motor von {this.Name} wurde gestartet");
+            }
+        }
+
+        // StoppeMotor
+        public void StoppeMotor()
+        {
+            if(!this.MotorLaeuft)
+                Console.WriteLine($"Der Motor von {this.Name} ist bereits gestoppt");
+            else if (this.AktGeschwindigkeit > 0)
+                Console.WriteLine($"Der Motor kann nicht gestoppt werden, da sich {this.Name} noch bewegt");
+            else
+            {
+                this.MotorLaeuft = false;
+                Console.WriteLine($"Der Motor von {this.Name} wurde gestoppt");
+            }
+        }
+
+        // Beschleunigen
+        public void Beschleunigen(int a)
+        {
+            if(this.MotorLaeuft)
+            {
+                if (this.AktGeschwindigkeit + a > this.MaxGeschwindigkeit)
+                    this.AktGeschwindigkeit = this.MaxGeschwindigkeit;
+                else if (this.AktGeschwindigkeit + a < 0)
+                    this.AktGeschwindigkeit = 0;
+                else 
+                    this.AktGeschwindigkeit += a;
+                Console.WriteLine($"{this.Name} bewegt sich jetzt mit {this.AktGeschwindigkeit}km/h");
+            }
+        }
     }
 
     public class Program
@@ -11,6 +76,22 @@
         {
             // Ändern des durch Console verwendetetn Zeichensatzes auf Unicode (damit das €-Symbol angezeigt werde kann)
             Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+            Fahrzeug fz1 = new Fahrzeug("BMW", 300, 10000);
+            Console.WriteLine(fz1.Info() + "\n");
+
+            // Methodenausführungen
+            fz1.StarteMotor();
+            fz1.Beschleunigen(120);
+            Console.WriteLine(fz1.Info() + "\n");
+
+            fz1.Beschleunigen(300);
+            Console.WriteLine(fz1.Info() + "\n");
+
+            fz1.Beschleunigen(-500);
+            fz1.StoppeMotor();
+            Console.WriteLine(fz1.Info() + "\n");
+
         }
     }
 }
---
+++ M007_GC_Static/Program.cs
@@ -0,0 +1,83 @@
+﻿namespace M007_GC_Static;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        // Static Methode
+        // Eine Methode als Static deklarieren, dann kann ich sie OHNE ein Objekt der Klasse aufrufen
+        int summe = Mathe.Adddiere(3, 5);
+        Console.WriteLine(summe);
+
+        // Statische Variable
+        // eine Static Variable wird von allen Objekten geteilt
+        new Auto();
+        new Auto();
+        new Auto();
+        Auto.AnzahlAutos += 1;
+        Console.WriteLine(Auto.AnzahlAutos);
+
+        // Statische Klasse
+        // eine static-Klasse kann nicht instanziiert werden (keine Objekterstellung möglich)
+        // Wofür sinnvoll?: Eine Sammlung von statischen Methoden
+        //Helfer h1 = new Helfer();
+        Helfer.ZeigeText();
+
+
+        Person person = new Person("Test");
+        person = null; // Setzt das Objekt auf null == Speicher frei geben, Objekt zerstören
+
+        for (int i = 0; i < 5; i++) 
+        {
+            Person p = new Person("Test");
+            Person p1 = new Person("Test");
+            //p = null;
+            //p1 = null;
+        }
+
+        //p1 = null; // Setzt das Objekt auf null == Speicher frei geben, Objekt zerstören
+
+        GC.Collect(); // Erzwingt eine Garbage Collection
+        GC.WaitForPendingFinalizers(); // Wartet auf die Abarbeitung des Destruktors
+        Console.WriteLine("Programm beendet");
+    }
+}
+
+class Mathe
+{
+    public static int Adddiere(int a, int b)
+    {
+        return a + b;
+    }
+}
+
+class Auto
+{
+    public static int AnzahlAutos = 0;
+    public Auto()
+    {
+        AnzahlAutos++;
+    }
+}
+
+static class Helfer
+{
+    public static void ZeigeText()
+    {
+        Console.WriteLine("Hallo!");
+    }
+}
+
+public class Person
+{
+    public string Vorname { get; set; }
+    public Person(string vorname)
+    {
+        Vorname = vorname;
+    }
+
+    ~Person()
+    {
+        Console.WriteLine("Objekt wurde zerstört");
+    }
+}
